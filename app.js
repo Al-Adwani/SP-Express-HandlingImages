56a5e041e936c47e5fe5feb5a32d8297518bffc6
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-11-01 16:35:30 +0300

    Task handling img backend  Done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const productRoutes = require("./apis/products/routes");
-const connectDB = require("./db/database");
+const connectDB = require("./database");
 const morgan = require("morgan");
 const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +13,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(logger);
 app.use((req, res, next) => {
@@ -18,10 +21,11 @@ app.use((req, res, next) => {
     res.status(400).json({ message: "I HATE BROCCOLI!! KEEFY! " });
   else next();
 });
-
+console.log("hii", path.join(__dirname, "media"));
 // Routes
-app.use("/api/products", productRoutes);
 
+app.use("/api/products", productRoutes);
+app.use("/media", express.static(path.join(__dirname, "media")));
 app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
 });
